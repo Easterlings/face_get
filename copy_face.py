b8c890cf745b68005614fe1734778e2cc6828e42
Easterlings <1299171412@qq.com>
2023-12-11 11:52:38 +0800

    添加两条保存路径，简化大部分内容

--- copy_face.py
+++ copy_face.py
@@ -1,42 +1,21 @@
-import copy
 import os
-import struct
-
 import cv2
-import numpy as np
-import supervision as sv
-import pandas as pd
 import torch
 import torchvision
-from PIL.Image import Image
-import sqlite3
 
 from local_groundingdino.util.inference import Model
-from segment_anything import sam_model_registry, SamPredictor
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # GroundingDINO config and checkpoint
 GROUNDING_DINO_CONFIG_PATH = "/ssd/cjy/saiwei-wardrobe/models/grounding-dino/GroundingDINO_SwinT_OGC.py"
 GROUNDING_DINO_CHECKPOINT_PATH = "/ssd/cjy/saiwei-wardrobe/models/grounding-dino/groundingdino_swint_ogc.pth"
-print(DEVICE)
-# Segment-Anything checkpoint
-SAM_ENCODER_VERSION = "vit_h"
-SAM_CHECKPOINT_PATH = "/ssd/cjy/saiwei-wardrobe/models/sam/sam_vit_h_4b8939.pth"
 
 # Building GroundingDINO inference model
 grounding_dino_model = Model(model_config_path=GROUNDING_DINO_CONFIG_PATH,
                              model_checkpoint_path=GROUNDING_DINO_CHECKPOINT_PATH, device=DEVICE)
 
-# # Building SAM Model and SAM Predictor
-# sam = sam_model_registry[SAM_ENCODER_VERSION](checkpoint=SAM_CHECKPOINT_PATH)
-# sam.to(device=DEVICE)
-# sam_predictor = SamPredictor(sam)
-
-# Predict classes and hyper-param for GroundingDINO
-# SOURCE_IMAGE_PATH = "./assets/my21.jpg"
 SOURCE_IMAGE_PATH = "./imgs/img"
-# CLASSES = ["The running dog"]
 BOX_THRESHOLD = 0.50
 CLASSES = ["Face"]
 TEXT_THRESHOLD = 0.50
@@ -56,16 +35,8 @@ def indexOfMaxConfidence(confidences):
 
 
 def face_only(sourceDir, imgDir, imgFile):
-    # data1 = isExist(conn, imgDir, imgFile)
-    # if data1.size > 0:
-    #     return
-    name="10_Hanna-4"
-    filename = imgFile[0:imgFile.rfind('.')]
-    # dirPath = os.path.join("imgs/faces/", imgDir)
-    boxDirPath = os.path.join(f"/data/lkw/train_resources/{name}", imgDir)
-    # if not os.path.exists(dirPath):
-    #     os.makedirs(dirPath)
-    #     os.makedirs(boxDirPath)
+    DirPath = os.path.join("./imgs/faces", imgDir)
+    TRDirPath = os.path.join(f"/data/lkw/train_resources", imgDir)
     image = cv2.imread(os.path.join(sourceDir, imgFile))
     detections = grounding_dino_model.predict_with_classes(
         image=image,
@@ -85,29 +56,22 @@ def face_only(sourceDir, imgDir, imgFile):
     print(f"After NMS: {len(detections.xyxy)} boxes")
     index = indexOfMaxConfidence(detections.confidence)
     if index < 0:
-        # insert(conn, (imgDir, imgFile, 0, "False"))
         return
-    # detections.mask = segment(
-    #     sam_predictor=sam_predictor,
-    #     image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB),
-    #     xyxy=detections.xyxy
-    # )
-    # segment_image = copy.copy(image)
-    # masked_image = mask_image(segment_image, detections.mask[index])
 
-    if not os.path.exists(boxDirPath):
-        # os.makedirs(dirPath)
-        os.makedirs(boxDirPath)
-    # filepath = os.path.join(dirPath, filename + '.png')
-    boxFilepath = os.path.join(boxDirPath, imgFile)
-    # save_masked_image(masked_image, filepath)
+    if not os.path.exists(TRDirPath):
+        os.makedirs(TRDirPath)
+    if not os.path.exists(DirPath):
+        os.makedirs(DirPath)
+    localFilepath = os.path.join(DirPath, imgFile)
+    boxFilepath = os.path.join(TRDirPath, imgFile)
+
     cropBox = detections.xyxy[index]
-    print(cropBox)
-    cropBox = square(cropBox)
-    print(cropBox)
+
+    cropBox = square(cropBox)#调整为方形
+
     cropImage = image[int(cropBox[1]):int(cropBox[3]), int(cropBox[0]): int(cropBox[2])]#y1 y2 x1 x2
+    cv2.imwrite(localFilepath, cropImage)
     cv2.imwrite(boxFilepath, cropImage)
-    # insert(conn, (imgDir, imgFile, detections.confidence[index], "True"))
 
 def square(cropBox):
     xlength= int(cropBox[2])-int(cropBox[0])
@@ -120,119 +84,9 @@ def square(cropBox):
         n = (xlength-ylength)/2
         cropBox[3]+=n
         cropBox[1]-=n
+    cropBox = [0 if num < 0 else num for num in cropBox]
     return cropBox
 
-def segment_image(image, segmentation_mask):
-    image_array = np.array(image)
-    segmented_image_array = np.zeros_like(image_array)
-    segmented_image_array[segmentation_mask] = image_array[segmentation_mask]
-    segmented_image = Image.fromarray(segmented_image_array)
-    black_image = Image.new("RGBA", image.size, (0, 0, 0, 0))
-    transparency_mask = np.zeros_like(segmentation_mask, dtype=np.uint8)
-    transparency_mask[segmentation_mask] = 255
-    transparency_mask_image = Image.fromarray(transparency_mask, mode='L')
-    black_image.paste(segmented_image, mask=transparency_mask_image)
-    return black_image
-
-
-def apply_mask(image, mask, alpha_channel=True):  # 应用并且响应mask
-    if alpha_channel:
-        alpha = np.zeros_like(image[..., 0])  # 制作掩体
-        alpha[mask == 1] = 255  # 兴趣地方标记为1，且为白色
-        image = cv2.merge((image[..., 0], image[..., 1], image[..., 2], alpha))  # 融合图像
-    else:
-        image = np.where(mask[..., None] == 1, image, 0)
-    return image
-
-
-def mask_image(image, mask, crop_mode_=True):  # 保存掩盖部分的图像（感兴趣的图像）
-    if crop_mode_:
-        y, x = np.where(mask)
-        y_min, y_max, x_min, x_max = y.min(), y.max(), x.min(), x.max()
-        cropped_mask = mask[y_min:y_max + 1, x_min:x_max + 1]
-        cropped_image = image[y_min:y_max + 1, x_min:x_max + 1]
-        masked_image = apply_mask(cropped_image, cropped_mask)
-    else:
-        masked_image = apply_mask(image, mask)
-
-    return masked_image
-
-
-def save_masked_image(image, filepath):
-    if image.shape[-1] == 4:
-        cv2.imwrite(filepath, image, [cv2.IMWRITE_PNG_COMPRESSION, 9])
-    else:
-        cv2.imwrite(filepath, image)
-    print(f"Saved as {filepath}")
-
-
-# Prompting SAM with detected boxes
-def segment(sam_predictor: SamPredictor, image: np.ndarray, xyxy: np.ndarray) -> np.ndarray:
-    sam_predictor.set_image(image)
-    result_masks = []
-    for box in xyxy:
-        masks, scores, logits = sam_predictor.predict(
-            box=box,
-            multimask_output=True
-        )
-        index = np.argmax(scores)
-        result_masks.append(masks[index])
-    return np.array(result_masks)
-
-
-# 打开或者创建数据库
-def openDB(dbFile):
-    if os.path.isfile(dbFile):
-        conn = sqlite3.connect(dbFile)
-    else:
-        conn = sqlite3.connect(dbFile)
-        cursor = conn.cursor()
-        cursor.execute('''
-			CREATE TABLE IMAGES
-			(ID INTEGER PRIMARY KEY AUTOINCREMENT,
-			GOODSID TEXT NOT NULL,
-			NAME TEXT NOT NULL,
-			SCORE FLOAT,
-			EXPORT TEXT);
-			''')
-        cursor.close()
-        conn.commit()
-    return conn
-
-
-# 插入数据库
-def insert(conn, parameters):
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO IMAGES (GOODSID,NAME,SCORE,EXPORT) VALUES (?,?,?,?)", parameters)
-    cursor.close()
-    conn.commit()
-
-
-# 关闭数据库
-def closeDB(conn):
-    conn.close()
-
-
-# 导出数据库保存到excel
-def export(conn, resultFile):
-    # print(struct.unpack('f',nani[3]))
-    data = pd.read_sql_query("SELECT * FROM IMAGES WHERE SCORE > 0", conn)
-    data.columns = ['ID', '商品id', '名字', '评分', '是否抠图']
-    data['评分'] = data['评分'].apply(lambda x: struct.unpack('f', x)[0])
-    data.to_excel(resultFile)
-
-
-# 导出数据库保存到excel
-def isExist(conn, imgDir, imgFile):
-    data = pd.read_sql_query("SELECT GOODSID,NAME FROM IMAGES WHERE GOODSID='{}' AND NAME='{}'".format(imgDir, imgFile),
-                             conn)
-    return data
-
-
-# dbName = "images.db"
-# conn = openDB(dbName)
-# export(conn, "images.xlsx")
-# closeDB(conn)
 
 for dir in os.listdir(SOURCE_IMAGE_PATH):
     sourceDir = os.path.join(SOURCE_IMAGE_PATH, dir)
@@ -241,5 +95,3 @@ for dir in os.listdir(SOURCE_IMAGE_PATH):
     for filename in os.listdir(sourceDir):
         print(filename)
         face_only(sourceDir, dir, filename)
-# export(conn, "images.xlsx")
-# closeDB(conn)
